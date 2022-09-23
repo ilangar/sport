f3b0a71f89acc3f50430c2adc14cde62ae2a3628
LucasBehrend <lucasbehrend2006@gmail.com>
2022-09-23 14:38:59 -0300

    connection string inicio

--- Sport1/Sport1/Inicio.cs
+++ Sport1/Sport1/Inicio.cs
@@ -17,7 +17,8 @@ namespace Sport1
         int pos = 38;
         int cantPerfiles;
         string[] nombres = new string[1000];
-        
+        OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Sport1-DB.accdb");
+
 
 
 
@@ -56,22 +57,23 @@ namespace Sport1
 
         private void Inicio_Load(object sender, EventArgs e)
         {
-            
-            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Sport1-DB.accdb";
-            SqlConnection cnn = new SqlConnection(connectionString);
-            SqlCommand command;
-            SqlDataReader dataReader;
-            String sql, Output = "";
-            sql = "select IdCrear, Nombre from Crear";
-            command = new SqlCommand(sql, cnn);
-            dataReader = command.ExecuteReader();
-            while (dataReader.Read())
+            connection.Open(); 
+
+
+
+
+            OleDbCommand info;
+            info = new OleDbCommand("Select IdCrear, Nombre FROM Crear", connection);
+            OleDbDataAdapter da = new OleDbDataAdapter(info);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Crear");
+
+            for(int i = 1; i<ds.Tables["Crear"].Rows.Count; i++)
             {
-                Output = Output + dataReader.GetValue(0) + "-" + dataReader.GetValue(1) + "/n";
+
             }
-
-            MessageBox.Show(Output);
-
+            
+            //MessageBox.Show("id: "+ ds.Tables["Crear"].Rows[0][0]  + " texto: " + ds.Tables["Crear"].Rows[0][1]);
 
 
         }
