de048fc8b6c481bb8872f9404d41ab2053a75f9f
LucasBehrend <lucasbehrend2006@gmail.com>
2022-09-28 11:23:40 -0300

    Crear Botones Inicio

--- Sport1/Sport1/CrearPerfil1.cs
+++ Sport1/Sport1/CrearPerfil1.cs
@@ -29,7 +29,7 @@ namespace Sport1
             connection.Open();
             OleDbCommand command = new OleDbCommand();
             command.Connection = connection;
-            command.CommandText = "insert into Crear (Nombre, Deporte, Rol) values ('" + nombre + "','" + deporte + "','" + rol + "')";
+            command.CommandText = "insert into Perfil (Nombre, Deporte, Rol) values ('" + nombre + "','" + deporte + "','" + rol + "')";
             command.ExecuteNonQuery();
             connection.Close();
             this.Hide();
--- Sport1/Sport1/Inicio.cs
+++ Sport1/Sport1/Inicio.cs
@@ -63,14 +63,21 @@ namespace Sport1
 
 
             OleDbCommand info;
-            info = new OleDbCommand("Select IdCrear, Nombre FROM Crear", connection);
+            info = new OleDbCommand("Select IdUser, Nombre FROM Perfil", connection);
             OleDbDataAdapter da = new OleDbDataAdapter(info);
             DataSet ds = new DataSet();
-            da.Fill(ds, "Crear");
+            da.Fill(ds, "Perfil");
 
-            for(int i = 1; i<ds.Tables["Crear"].Rows.Count; i++)
+            for(int i = 1; i<ds.Tables["Perfil"].Rows.Count; i++)
             {
-
+                Button btnPerfil1 = new Button();
+                btnPerfil1.DialogResult = DialogResult.OK;
+                btnPerfil1.Text = nombres[cantPerfiles - 1];
+                btnPerfil1.Location = new Point(24, pos);
+                pos += 100;
+                btnPerfil1.Size = new Size(160, 70);
+                btnPerfil1.Click += new System.EventHandler(this.click_btn_perfil);
+                Controls.Add(btnPerfil1);
             }
             
             //MessageBox.Show("id: "+ ds.Tables["Crear"].Rows[0][0]  + " texto: " + ds.Tables["Crear"].Rows[0][1]);
