3cede22405bf012fee726a239920d0207561a469
LucasBehrend <lucasbehrend2006@gmail.com>
2022-10-12 12:06:41 -0300

    .

---
+++ Sport1/Sport1/perfilEnt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sport1
+{
+    public partial class perfilEnt : Form
+    {
+        public perfilEnt()
+        {
+            InitializeComponent();
+        }
+
+        private void PerfilEnt_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
