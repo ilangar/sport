9d128b2ad27c6ba8149d488b38a8b3e0ed723457
DemiGar <demiangarfunkel@gmail.com>
2022-10-25 10:37:02 -0300

    diseño

--- Sport1/Sport1/Form1.Designer.cs
+++ Sport1/Sport1/Form1.Designer.cs
@@ -33,22 +33,29 @@
             // 
             // btnComenzar
             // 
-            this.btnComenzar.Location = new System.Drawing.Point(584, 815);
-            this.btnComenzar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnComenzar.BackColor = System.Drawing.Color.White;
+            this.btnComenzar.BackgroundImage = global::Sport1.Properties.Resources.Botón__2_;
+            this.btnComenzar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnComenzar.FlatAppearance.BorderSize = 0;
+            this.btnComenzar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComenzar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnComenzar.Location = new System.Drawing.Point(279, 287);
+            this.btnComenzar.Margin = new System.Windows.Forms.Padding(0);
             this.btnComenzar.Name = "btnComenzar";
-            this.btnComenzar.Size = new System.Drawing.Size(268, 132);
+            this.btnComenzar.Size = new System.Drawing.Size(268, 65);
             this.btnComenzar.TabIndex = 0;
-            this.btnComenzar.Text = "Toca para comenzar";
-            this.btnComenzar.UseVisualStyleBackColor = true;
+            this.btnComenzar.UseVisualStyleBackColor = false;
             this.btnComenzar.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.BackgroundImage = global::Sport1.Properties.Resources.FONDO;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(824, 461);
             this.Controls.Add(this.btnComenzar);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
