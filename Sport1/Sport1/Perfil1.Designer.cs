1932de44a23ab8155d67fb3c356eda89e8af923e
ilangar <110467573+ilangar@users.noreply.github.com>
2022-11-04 15:06:17 -0300

    borrar perfil

--- Sport1/Sport1/Perfil1.Designer.cs
+++ Sport1/Sport1/Perfil1.Designer.cs
@@ -32,6 +32,7 @@
             this.btnIngresarEstadisticas = new System.Windows.Forms.Button();
             this.btnVerProgresos = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.eliminarPerfil = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnVolver
@@ -74,11 +75,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click_1);
             // 
+            // eliminarPerfil
+            // 
+            this.eliminarPerfil.Location = new System.Drawing.Point(859, 22);
+            this.eliminarPerfil.Name = "eliminarPerfil";
+            this.eliminarPerfil.Size = new System.Drawing.Size(120, 23);
+            this.eliminarPerfil.TabIndex = 5;
+            this.eliminarPerfil.Text = "Eliminar perfil";
+            this.eliminarPerfil.UseVisualStyleBackColor = true;
+            this.eliminarPerfil.Click += new System.EventHandler(this.EliminarPerfil_Click);
+            // 
             // Perfil1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.eliminarPerfil);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnVerProgresos);
             this.Controls.Add(this.btnIngresarEstadisticas);
@@ -97,5 +109,6 @@
         private System.Windows.Forms.Button btnIngresarEstadisticas;
         private System.Windows.Forms.Button btnVerProgresos;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button eliminarPerfil;
     }
 }
