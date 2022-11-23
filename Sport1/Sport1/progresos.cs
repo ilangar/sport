e5a499690a48429930a3a57940ceced0b5fafb48
ilangar <110467573+ilangar@users.noreply.github.com>
2022-11-23 01:45:49 -0300

    pusheo

--- Sport1/Sport1/progresos.cs
+++ Sport1/Sport1/progresos.cs
@@ -1177,7 +1177,7 @@ namespace Sport1
             OleDbCommand info3;
             OleDbCommand infobj;
 
-            info = new OleDbCommand("Select Estd, IdCar, Fecha FROM IngresarEstadisticas WHERE Per ='" + Convert.ToString(Program.idPerfil) + "'", connection);
+            info = new OleDbCommand("Select Estd, IdCar, Fecha FROM IngresarEstadisticas WHERE IdJugadorEquipo ='" + Convert.ToString(Program.idPerfil) + "'", connection);
             info2 = new OleDbCommand("Select Deporte FROM Perfil WHERE Nombre = '" + Program.idPerfil + "'", connection);
             info3 = new OleDbCommand("Select Deporte FROM JugadorEquipo Where IdUser= " + Convert.ToInt32(Program.idUser) , connection);
             infobj = new OleDbCommand("Select Objetivo, IdCar FROM Obj WHERE Per =" + Program.idUser, connection) ;
